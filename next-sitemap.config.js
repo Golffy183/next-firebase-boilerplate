b9c83ad16884a287c37f51cb76c81131c8734a3b
Golffy183 <kowit.narongtuwapan@hotmail.com>
2024-04-03 16:51:59 +0700

    update: next-sitemap.config.js and sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://example.com',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [`${process.env.SITE_URL}/sitemap.xml`],
+  },
 };
